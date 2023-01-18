0ae0f113f944139eaf86c5613aba4d621126d8f7
SPCRi07 <solankiparth115@gmail.com>
2023-01-18 22:36:39 +0530

    all commits

--- app/src/main/java/com/simform/androidpractise/MainActivity.java
+++ app/src/main/java/com/simform/androidpractise/MainActivity.java
@@ -3,12 +3,25 @@ package com.simform.androidpractise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="hello" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: 1");
+
+        Log.d(TAG, "onCreate: 2");
+
+        Log.d(TAG, "onCreate: 3");
+
+        Log.d(TAG, "onCreate: 4");
+
+        Log.d(TAG, "onCreate: 5");
     }
 }
